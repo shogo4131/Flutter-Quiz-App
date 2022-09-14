29295bc339c1f18fff45caeaccd1b81b1e75b41c
shogo4131 <shougo08697118@gmail.com>
2022-09-14 23:23:30 +0900

    feat: add anwser button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fl_study/answer.dart';
 import 'package:fl_study/question.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var questions = ['あなたの好きな色は？', 'あなたの好きな食べ物は？'];
+  var questions = [
+    {
+      'questionText': 'あなたの好きな色は？',
+      'anwsers': ['黒', '赤', '白'],
+    },
+    {
+      'questionText': 'あなたの好きな食べ物は？',
+      'anwsers': ['カレー', 'マグロ', ' オムライス'],
+    },
+    {
+      'questionText': 'あなたの好きな食べ物は？',
+      'anwsers': ['カレー', 'マグロ', ' オムライス'],
+    }
+  ];
 
   int _questionIndex = 0;
 
@@ -17,8 +31,6 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-
-    print(_questionIndex);
   }
 
   @override
@@ -26,21 +38,16 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My APP bar')),
-        body: Column(children: [
-          Question(questions[_questionIndex]),
-          ElevatedButton(
-            child: Text('Anwser 1'),
-            onPressed: _answerQuestion,
-          ),
-          ElevatedButton(
-            child: Text('Anwser 2'),
-            onPressed: _answerQuestion,
-          ),
-          ElevatedButton(
-            child: Text('Anwser 3'),
-            onPressed: _answerQuestion,
-          ),
-        ]),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'] as String,
+            ),
+            ...(questions[_questionIndex]['anwsers'] as List<String>)
+                .map((answser) => Answer(_answerQuestion, answser))
+                .toList()
+          ],
+        ),
       ),
     );
   }
